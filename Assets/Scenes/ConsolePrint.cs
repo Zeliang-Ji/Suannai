9c0e4d915658568631ff5a25aa1b0362487f05c4
Zeliang Ji <Zeliang.Ji-1@student.uts.edu.au>
2022-08-27 17:45:41 +0800

    80 percent complete

---
+++ Assets/Scenes/ConsolePrint.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsolePrint : MonoBehaviour
+{
+    private int i = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //int i = 3;
+        Debug.Log(gameObject.name + ":" + i++);
+
+    }
+}
