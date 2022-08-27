27c347e414a47c82b5baf4821c59c298881c83ce
Zeliang Ji <Zeliang.Ji-1@student.uts.edu.au>
2022-08-27 19:08:22 +0800

    80 percent complete

---
+++ Assets/Scenes/PrintAndHide.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintAndHide : MonoBehaviour
+{
+    public Renderer rend;
+    int i =3;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(gameObject.name + ":"+ ++i);
+        
+    }
+
+}
